58db2039c355c826f352c679e53ad051e56eaba1
kabilmdk <kabilmdk27@gmail.com>
2025-10-23 01:29:39 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBBUGtawvVqiGTk-Msrp_LpZKc7H6KRiRk",
+  authDomain: "crackerstest-35a1a.firebaseapp.com",
+  projectId: "crackerstest-35a1a",
+  storageBucket: "crackerstest-35a1a.firebasestorage.app",
+  messagingSenderId: "247941197745",
+  appId: "1:247941197745:web:c63f3755b6230ad32d9790",
+  measurementId: "G-VYFE33E2BV"
+};
